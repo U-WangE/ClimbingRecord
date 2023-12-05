7910fbd6fe712c4695bd4b79dab3a2a3d51b2b3b
U-WangE <chamchamm@naver.com>
2023-12-05 18:24:55 +0900

    [add]kakao map 적용, base activity, map activity, record activity 추가

--- app/src/main/java/com/ihavesookchi/climbingrecord/RecordActivity.kt
+++ app/src/main/java/com/ihavesookchi/climbingrecord/RecordActivity.kt
@@ -3,9 +3,9 @@ package com.ihavesookchi.climbingrecord
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RecordActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_record)
     }
 }
