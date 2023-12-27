22932fd9fd2c3345d656a5b7433976e91142ce70
U-WangE <chamchamm@naver.com>
2023-12-27 14:13:09 +0900

    [add]BaseViewModel 생성, MapFragment에 SharedViewModel 적용

---
+++ app/src/main/java/com/ihavesookchi/climbingrecord/data/Dummy.kt
@@ -0,0 +1,23 @@
+package com.ihavesookchi.climbingrecord.data
+
+import com.ihavesookchi.climbingrecord.data.response.SearchKeyWordResponse
+
+object Dummy {
+//    fun getSearchKeyWordResponse(): SearchKeyWordResponse {
+//        return SearchKeyWordResponse(
+//            meta = SearchKeyWordResponse.Meta(
+//                totalCount =
+//            ),
+//            document = listOf(
+//                SearchKeyWordResponse.Document(
+//                    id = ,
+//                    placeName = ,
+//                    addressName = ,
+//                    roadAddressName = ,
+//                    x = ,
+//                    y =
+//                )
+//            )
+//        )
+//    }
+}
