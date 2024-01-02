9c800b5cb3270070cfce7d36f4beec8b1743f68e
U-WangE <chamchamm@naver.com>
2024-01-02 19:02:00 +0900

    [delete]retorift respoonse flow 로 처리하는 로직 제거, [update]repository에서 viewmodel의 역할인 데이터 관리, 처리 이벤트를 하는 것 같아 변경,

--- app/src/main/java/com/ihavesookchi/climbingrecord/viewModel/BaseViewModel.kt
+++ app/src/main/java/com/ihavesookchi/climbingrecord/viewModel/BaseViewModel.kt
@@ -1,32 +1,40 @@
 package com.ihavesookchi.climbingrecord.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ihavesookchi.climbingrecord.ClimbingRecordLogger
 import com.ihavesookchi.climbingrecord.data.repository.SearchRepository
+import com.ihavesookchi.climbingrecord.data.response.SearchKeywordResponse
 import com.ihavesookchi.climbingrecord.data.uistate.SearchKeywordUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class BaseViewModel @Inject constructor(
     private val searchRepository: SearchRepository
 ): ViewModel() {
+        private var _searchKeywordUiState: MutableLiveData<SearchKeywordUiState> = MutableLiveData()
+        val searchKeywordUiState: MutableLiveData<SearchKeywordUiState> get() = _searchKeywordUiState
 
     fun searchKeywordApi(keyword: String) {
-        viewModelScope.launch {
-            searchRepository.searchKeywordApi(keyword)
-                .collect { uiState ->
-                    when (uiState) {
-                        is SearchKeywordUiState.SearchKeywordSuccess -> {
-
+        viewModelScope.launch(Dispatchers.IO) {
+            searchRepository.searchKeywordApi(keyword).let {
+                launch(Dispatchers.Main) {
+                    if (it.isSuccessful) {
+                        withContext(Dispatchers.IO) {
+                            searchRepository.setSearchKeywordResponse(it.body())
                         }
-
-                        is SearchKeywordUiState.SearchKeywordFailure -> {
-                        }
-                        else -> {}
+                        _searchKeywordUiState.value = SearchKeywordUiState.SearchKeywordSuccess
+                    }
+                    else {
+                        _searchKeywordUiState.value = SearchKeywordUiState.SearchKeywordFailure
                     }
                 }
+            }
         }
     }
 }
