1ad6001cfab89f06d36aeedd27fbbc593d04262d
U-WangE <chamchann@gmail.com>
2025-10-17 15:21:26 +0900

    build: keystore 생성, signingConfigs release type 생성 및 keystore 정보 연결 및 release build 설정 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("climbingrecord.android.application")
     id("climbingrecord.android.compose")
@@ -10,6 +12,26 @@ android {
         versionCode = 1
         versionName = "1.0.0"
         applicationId = "com.uwange.climbingrecord"
+    }
+
+    signingConfigs {
+        val keystoreProperties = Properties()
+        keystoreProperties.load(project.rootProject.file("keystore.properties").bufferedReader())
+
+        create("release") {
+            storeFile = file(keystoreProperties["STORE_FILE_PATH"] as String)
+            storePassword = keystoreProperties["STORE_PASSWORD"] as String
+            keyAlias = keystoreProperties["KEY_ALIAS"] as String
+            keyPassword = keystoreProperties["STORE_PASSWORD"] as String
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs["release"]
+            isMinifyEnabled = true
+            isShrinkResources = true
+        }
     }
 }
 
