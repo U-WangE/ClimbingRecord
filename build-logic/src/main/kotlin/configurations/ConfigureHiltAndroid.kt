7865b67682f3ac716cd2158dd330393031e439a6
U-WangE <chamchann@gmail.com>
2025-10-24 12:12:20 +0900

    feat: LoginViewModel.kt 추가, feature plugin 에 kakao api 주입, feat: LoginScreen.kt 에 Login Button 추가, viewModel 추가, kakao login function 추가

--- build-logic/src/main/kotlin/configurations/ConfigureHiltAndroid.kt
+++ build-logic/src/main/kotlin/configurations/ConfigureHiltAndroid.kt
@@ -4,13 +4,14 @@ import org.gradle.kotlin.dsl.dependencies
 internal fun Project.configureHiltAndroid() {
     with(pluginManager) {
         apply("com.google.devtools.ksp")
-        apply("com.google.dagger.hilt.android")
+        apply("dagger.hilt.android.plugin")
     }
 
     val libs = extensions.libs
 
     dependencies {
         add("implementation", libs.findLibrary("hilt.android").get())
+        add("implementation", libs.findLibrary("hilt.navigation.compose").get())
         add("ksp", libs.findLibrary("hilt.android.compiler").get())
     }
 }
--- feature/auth/src/main/java/com/uwange/auth/graph/login/LoginScreen.kt
+++ feature/auth/src/main/java/com/uwange/auth/graph/login/LoginScreen.kt
@@ -1,18 +1,33 @@
 package com.uwange.auth.graph.login
 
+import android.content.Context
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.user.UserApiClient
 import com.uwange.climbingrecord.designsystem.R
 import com.uwange.designsystem.component.ClimbingRecordLoginButton
 
 @Composable
-internal fun LoginRoute() {
+internal fun LoginRoute(
+    viewModel: LoginViewModel = hiltViewModel()
+) {
     BackHandler {
+
+    }
+
+    LaunchedEffect(viewModel) {
 
     }
 
@@ -32,8 +47,36 @@ fun LoginScreen(
     ) {
         ClimbingRecordLoginButton(
             label = stringResource(R.string.kakao_login),
-            imageId = ,
+            imageId = R.drawable.ic_kakao_login,
             containerColor = Color(0xFFFEE500),
+            onClick = loginKakao,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         )
+    }
+}
+
+private fun loginKakao(
+    context: Context,
+    onSuccess: (String) -> Unit,
+    onFailure: (Throwable) -> Unit
+) {
+    val callBack: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null && error !is ClientError) {
+            onFailure(error)
+        } else if (token != null) {
+            onSuccess(token.accessToken)
+        }
+    }
+
+    UserApiClient.instance.apply {
+        if (isKakaoTalkLoginAvailable(context)) {
+            // 카카오톡 로그인 (앱)
+            loginWithKakaoTalk(context, callback = callBack)
+        } else {
+            // 카카오 계정 로그인 (웹)
+            loginWithKakaoAccount(context, callback = callBack)
+        }
     }
 }
